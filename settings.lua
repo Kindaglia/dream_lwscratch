4f389263b929fb7c2abe662b360bf5d62a62222a
Kindaglia <matteodrago007@gmail.com>
2023-05-26 13:39:56 +0200

    add custom robot

--- settings.lua
+++ settings.lua
@@ -4,13 +4,13 @@ local utils = ...
 utils.settings = { }
 
 utils.settings.running_tick =
-	tonumber(minetest.settings:get("lwscratch_running_tick") or 0.1)
+	tonumber(minetest.settings:get("lwscratch_running_tick") or 0)
 
 utils.settings.robot_move_delay =
-	tonumber(minetest.settings:get("lwscratch_robot_move_delay") or 0.5)
+	tonumber(minetest.settings:get("lwscratch_robot_move_delay") or 0)
 
 utils.settings.robot_action_delay =
-	tonumber(minetest.settings:get("lwscratch_robot_action_delay") or 0.2)
+	tonumber(minetest.settings:get("lwscratch_robot_action_delay") or 0)
 
 utils.settings.public_chat =
 	minetest.settings:get_bool ("lwscratch_allow_public_chat", true)
@@ -21,12 +21,12 @@ utils.settings.use_mod_on_place =
 utils.settings.alert_handler_errors =
 	minetest.settings:get_bool ("lwscratch_alert_handler_errors", true)
 
-if utils.settings.robot_move_delay < 0.1 then
-	utils.settings.robot_move_delay = 0.1
+if utils.settings.robot_move_delay < 0 then
+	utils.settings.robot_move_delay = 0
 end
 
-if utils.settings.robot_action_delay < 0.1 then
-	utils.settings.robot_action_delay = 0.1
+if utils.settings.robot_action_delay < 0 then
+	utils.settings.robot_action_delay = 0
 end
 
 utils.settings.default_stack_max =
